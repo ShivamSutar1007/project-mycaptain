6b552d28cbb9a3ea819ac77203ff52dcc44a9559
ShivamSutar1007 <115448103+ShivamSutar1007@users.noreply.github.com>
2022-10-10 16:18:13 +0530

    Add files via upload

---
+++ second_task.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+filename = input("entre file name "); 
+f = filename.split(".")
+print ("Extension of the file is : ",f[-1])
+
---
+++ take_one.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pi = 3.14
+radius =float(input("entre the radius of the cirlce "));
+area = pi * radius * radius 
+print("the area of the circle is = ",area);
+
