2ce4486e918af46715354cfb4582b86efc0baffb
ShivamSutar1007 <115448103+ShivamSutar1007@users.noreply.github.com>
2022-10-21 00:07:03 +0530

    grade calculator, task 4

---
+++ grade calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+print("enter yoru marks to get yoru grades ");
+x=int(input());
+
+if x>90:
+    print("grade A")
+elif x>75:
+    print("grade B")
+elif x>50:
+    print("grade C")
+else:
+    print("fail")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
