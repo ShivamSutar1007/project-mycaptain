7a920d017ffe817391e404a20d919d4ee8d6d25b
ShivamSutar1007 <115448103+ShivamSutar1007@users.noreply.github.com>
2022-10-21 19:11:26 +0530

    Add files via upload

---
+++ print even number.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+# Python program to print Even Numbers in a List
+list = [20, 31, 8, 94, 62, 83];
+for i in list:
+    if i % 2 == 0:
+        print(i, end=" ");
+
+
+# In[ ]:
+
+
+
+
